a55c2ca82096be93cd010db6fe02bc464df3505c
Leonardo Val <leonardo.val@creatartis.com>
2015-02-19 18:58:58 -0200

    Jobs array to generator.
    + Changed the jobs array for a generator.
    + Also added many TODOs and FIXMEs.

--- src/main.js
+++ src/main.js
@@ -1,41 +1,45 @@
 "use strict",
+// Imports
 require("source-map-support").install();
 var capataz = require('capataz'),
     base = capataz.dependencies.base,
-    server = capataz.Capataz.run({
-        port: 80
-			  customFiles: './'
-    });
-var jobs = [];
-var config = require("src/scene.json");
-var jobfunction(x, y)
-{
-	var raytracer = new Raytracer();
-	return raytracer.execute(x,y);
+	config = require("src/scene.json");
+	
+// Server setup.
+var server = capataz.Capataz.run({
+	port: 80
+	customFiles: './' //FIXME Mover a un directorio específico
+});
+
+var jobFunction = function jobfunction(jsraytracer, x, y) {
+	var raytracer = new jsraytracer.Raytracer(),
+		pixel = raytracer.execute(x,y);
+	return { x: x, y: y, colour: pixel.toRGB() }; //TODO Método Color.toRGB()
 }
-for(var x = 0; x<config.canvasWidth; x++)
-{
-	for(var y = 0; y<config.canvasHeight; y++)
-	{
-		jobs.push( {
-			info: "(x,y): ("+x+","+y+")",
+
+var jobs = base.Iterable.range(config.canvasWidth)
+	.zip(base.Iterable.range(config.canvasHeight))
+	.mapApply(function (x,y) {
+		return {
+			info: "("+ x +","+ y +")",
 			fun: jobfunction,
 			imports: ["jsraytracer"]
 			args: [x, y]
-		});
-	}
-}
+		};
+	});
 
-server.logger.info("Starting... ", "now" ));
-server.scheduleAll(jobs, 1000, function (scheduled) {
+server.logger.info("Starting server.");
+server.scheduleAll(jobs, 2000, function (scheduled) {
 	return scheduled.then(function (result) {
-		server.logger.info(result);
+		//server.logger.info(result);
+		//TODO Guardar pixel en el canvas.
 		return true;
-		}
+	}
 });
 }).then(function () {
-	    server.logger.info("Finished. Stopping server.");
-			    process.exit();
+	//TODO Guardar canvas a archivo.
+	server.logger.info("Finished. Stopping server.");
+	process.exit();
 });
 
 
